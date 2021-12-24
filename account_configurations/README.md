1aef081fbdb9d6d64611465fe9c16411d0d09caf
Oskari Niskanen <oskari.niskanen97@gmail.com>
2021-12-25 00:48:38 +0200

    Added automatic account configuration creation.

---
+++ account_configurations/README.md
@@ -0,0 +1,6 @@
+This folder is used to hold account configuration files.
+Each account has their own configuration file, where hashtags and quotes can be inserted.
+Also, account name, page identifier and Instagram user id will be stored here to reduce API call traffic.
+
+These files can be created automatically by calling the application with the flag -a.
+After the files are created, you can insert your own custom hashtags and captions to the arrays.s
--- src/run_gatherer.py
+++ src/run_gatherer.py
@@ -1,11 +1,14 @@
 
 import requests
 import logging
+import json
+from pathlib import Path
 import parse_config
 from image_supplier import ImageSupplier
 
 
-ACCOUNT_CONFIG_PATH = "../account_configurations/"
+# Account configuration file path
+ACCOUNT_CONFIG_PATH = str(Path().resolve()) + "/account_configurations/"
 
 
 """ Class to handle calls to Instagram Graph API """
@@ -47,7 +50,7 @@ class GraphHandler:
     def get_business_user_ids(self):
         """ Gets Instagram Business User identifiers and appends them into the dictionary """
 
-        logging.info("Querying businessa account user identifiers.")
+        logging.info("Querying business account user identifiers.")
         if self.info['accounts']:
             # Setting payload to fetch Instagram business account id
             payload = {'access_token': self.args.graph_api_access_token,
@@ -68,6 +71,37 @@ class GraphHandler:
                         # Setting the received IG user id to the account's dict
                         account['user_id'] = resp_data['instagram_business_account']['id']
 
+    def create_configuration_files(self):
+        """ Creates/updates configuration files for each Instagram user. """
+
+        # Calling function that creates dictionary of user info
+        self.set_up_info()
+
+        if self.info:
+            # Iterating over each account
+            for account in self.info['accounts']:
+                # Constructing file name
+                file_name = ACCOUNT_CONFIG_PATH + account['user_id'] + ".json"
+                # If configuration file for the specific account already exists,
+                # open in r+ mode to avoid overwriting.
+                if Path(file_name).is_file():
+                    conf_file = open(file_name, 'r+')
+                    conf_data = json.load(conf_file)
+                    # Updating name, since only it can be changed.
+                    conf_data['name'] = account['name']
+                    # Moving pointer back to the beginning of the file.
+                    conf_file.seek(0)
+                    # Writing updated values
+                    json.dump(conf_data, conf_file, indent=4)
+                else:
+                    conf_file = open(file_name, 'w')
+                    # Adding 2 new fields to hold hashtags and captions.
+                    account["hashtags"] = []
+                    account["captions"] = []
+                    json.dump(account, conf_file, indent=4)
+                # Closing file
+                conf_file.close()
+
     def get_account_media_count(self):
         """ Fetches the count of posts on the accounts """
 
@@ -84,7 +118,7 @@ class GraphHandler:
                     logging.info(account['name'] + " has " +
                                  str(len(resp_data['data'])) + " posts.")
 
-    def publish_image_to_account(self, creation_id, user_id):
+    def publish_image(self, creation_id, user_id):
         """ Publishes given image to Instagram account. """
 
         logging.info("Publishing image to account with user id: " + user_id)
@@ -117,7 +151,7 @@ class GraphHandler:
             # Getting creation id as the response
             creation_id = resp.json()['id']
             # Calling publishing function
-            self.publish_image_to_account(creation_id, user_id)
+            self.publish_image(creation_id, user_id)
         else:
             logging.warning(
                 "Creation of media container failed. \n Response: " + resp.json())
@@ -127,6 +161,16 @@ class GraphHandler:
 
         self.get_account_info()
         self.get_business_user_ids()
+
+    def start_posting_process(self):
+        """ Starts the process of posting photos to each account."""
+
+        # Read configuration file into memory
+        for p in Path(ACCOUNT_CONFIG_PATH).glob('*.json'):
+            # Loading account configuration data into dictionary
+            account_data = json.loads(p.read_text())
+            logging.info(
+                "Starting posting process for account: " + account_data['name'])
 
 
 def main():
@@ -139,7 +183,13 @@ def main():
     args = parse_config.get_configuration()
     # Initialize Instagram Graph API handler
     handler = GraphHandler(args)
-    #handler.set_up_info()
+
+    # Checking if accounts need to be configured.
+    if args.configure_accounts:
+        handler.create_configuration_files()
+    else:
+        handler.start_posting_process()
+
     #image_supplier = ImageSupplier(args)
     #image_info = supplier.get_random_image_from_collections("2102317,9254430")
     #user_id = "17841450746190808"
